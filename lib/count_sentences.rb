c413125515f38a6703946bfcb1c112710dfe40ec
Vincent Servio <vincentservio@gmail.com>
2020-02-20 19:18:58 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,12 +17,21 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
    if self == ""
   
       0 
    else 
         #binding.pry 
   self.split(/[.!?]/).reject(&:empty?).count
+=======
+   if self.split(/[.!?]/).count == 0 
+     # binding.pry
+      0 
+   else 
+     
+  self.split(/[.!?]/).reject!(&:empty?).count
+>>>>>>> 87257118442900a0832bd068be976a1f88ade159
    
  end
 # binding.pry
